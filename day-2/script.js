197b992800941deb6844dd2b66514ba72b72b968
Mikel Edwin <michealanthonyxxx@gmail.com>
2023-09-02 12:39:01 +0100

    Initial Commit - Day 2

---
+++ day-2/script.js
@@ -0,0 +1,24 @@
+const { createApp, ref, reactive } = Vue;
+
+createApp({
+    setup(){
+        const imgArray = Array(4).fill(1).map((a,b) => a + b).map(a => ({
+            main: `./images/image-product-${a}.jpg`,
+            thumbnail: `./images/image-product-${a}-thumbnail.jpg`,
+            active: false,
+        }));
+        imgArray[0].active = true;
+        const images = reactive(imgArray);
+        const count = ref(0);
+        function active(img){
+            images.forEach(a => a.active = false);
+            img.active = true;
+        }
+        function n(x){
+            const index = images.indexOf(images.find(a => a.active));
+            const curr = x == '+' ? images[index + 1] || images[0] : images[index - 1] || images[images.length - 1];
+            return active(curr);
+        }
+        return { images, count, active, n }
+    }
+}).mount('main');
